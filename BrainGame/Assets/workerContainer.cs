c14bb6269480909279ecad74c7cbe8ce4ded6b4a
Singular Cabbage <daniellangamma@gmail.com>
2018-03-26 02:09:47 -0400

    Some work done
    Mostly UI, but I also added in a worker sprite and started on a script to add workers to boxes

---
+++ BrainGame/Assets/workerContainer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class workerContainer : MonoBehaviour {
+    public GameObject worker;
+
+
+	// Use this for initialization
+	void Start () {
+        addWorker();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void addWorker() {
+        //getting the dimensions of the gameObject
+        //thought it might be useful later
+        Vector3 objectPosition = transform.position;
+        Vector3 size = gameObject.GetComponent<SpriteRenderer>().bounds.size;
+
+        Debug.Log("gameObj size " + size);
+
+        Instantiate(worker).transform.position = objectPosition;
+    }
+}
