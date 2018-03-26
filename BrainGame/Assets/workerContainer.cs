aeb5e38d878ca1429bc2368b225f68badd5add31
Singular Cabbage <daniellangamma@gmail.com>
2018-03-26 15:38:48 -0400

    Swapped out the brain region sprite to an image
    The anchoring settings need some more work

--- BrainGame/Assets/workerContainer.cs
+++ BrainGame/Assets/workerContainer.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class workerContainer : MonoBehaviour {
     public GameObject worker;
+    public int maxWorkers = -1;
+    public int maxRows = -1;
 
+    private List<GameObject> workersList;
 
 	// Use this for initialization
 	void Start () {
+        //might have to delete this manually to prevent leaks?
+        workersList = new List<GameObject>();
+
+        //setup our default valules
+        if (maxWorkers == -1)
+            maxWorkers = 6;
+        if (maxRows == -1)
+            maxRows = 1;
+
         addWorker();
     }
 	
@@ -17,13 +29,16 @@ public class workerContainer : MonoBehaviour {
 	}
 
     void addWorker() {
-        //getting the dimensions of the gameObject
-        //thought it might be useful later
-        Vector3 objectPosition = transform.position;
-        Vector3 size = gameObject.GetComponent<SpriteRenderer>().bounds.size;
+        RectTransform rt = gameObject.GetComponent<RectTransform>();
+        Vector3 objectPosition = rt.position;
+        Vector2 objectSize = rt.rect.size;
 
-        Debug.Log("gameObj size " + size);
+        // TODO: add some fancy math that calculates optimal spacing for the specified amounts of workers
 
-        Instantiate(worker).transform.position = objectPosition;
+        //for now I'll just try this
+
+        GameObject workerObject =  Instantiate(worker);
+        workerObject.transform.position = objectPosition;
+        workersList.Add(workerObject);
     }
 }
