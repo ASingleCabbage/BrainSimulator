54785643987e32f0bb9d306d9a2d90e7c9cac278
Singular Cabbage <daniellangamma@gmail.com>
2018-03-27 16:57:47 -0400

    UPDATES
    Do not pull unless updated to latest version of Unity

---
+++ BrainGame/Assets/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Might want to move all button and other UI handling here */
+public class GameController : MonoBehaviour {
+    public GameObject workerDisplay;
+    public List<GameObject> brainRegions;   //adding in brianRegions will require more scripting or custom inspector
+
+    //so I'll just use a variable for now
+    public GameObject brainRegion;
+
+    public int maxWorker;
+    public string workerCountDisplayString;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //might want to update workerCount only when button presses
+        int usedWorkerCount = 0;
+        //foreach (GameObject region in brainRegions) {
+        //    usedWorkerCount += region.GetComponent<WorkerContainer>().getWorkerCount();
+        //}
+        usedWorkerCount = brainRegion.GetComponent<WorkerContainer>().getWorkerCount();
+        Debug.Log("setting text...");
+        workerDisplay.GetComponent<Text>().text = string.Format(workerCountDisplayString, usedWorkerCount, maxWorker);
+	}
+}
--- BrainGame/Assets/workerContainer.cs
+++ BrainGame/Assets/workerContainer.cs
@@ -1,44 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class workerContainer : MonoBehaviour {
+public class WorkerContainer : MonoBehaviour {
     public GameObject worker;
-    public int maxWorkers = -1;
-    public int maxRows = -1;
+    public float workerMargin;
+    public int maxRow;
 
     private List<GameObject> workersList;
+
+    public Button addButton;
+    public Button remButton;
 
 	// Use this for initialization
 	void Start () {
         //might have to delete this manually to prevent leaks?
         workersList = new List<GameObject>();
 
-        //setup our default valules
-        if (maxWorkers == -1)
-            maxWorkers = 6;
-        if (maxRows == -1)
-            maxRows = 1;
-
-        addWorker();
+        addButton.onClick.AddListener(() => addWorker());
+        remButton.onClick.AddListener(() => removeWorker());
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
     void addWorker() {
         RectTransform rt = gameObject.GetComponent<RectTransform>();
-        Vector3 objectPosition = rt.position;
-        Vector2 objectSize = rt.rect.size;
 
-        // TODO: add some fancy math that calculates optimal spacing for the specified amounts of workers
-
-        //for now I'll just try this
+        //getting local x and y position for worker based on the specified params
+        float yPos = rt.rect.yMin + rt.rect.height / (maxRow + 1);
+        float xPos = rt.rect.xMin + workerMargin * (workersList.Count + 1);
+        Vector3 workerLocalPos = new Vector3(xPos, yPos, 0);
 
         GameObject workerObject =  Instantiate(worker);
-        workerObject.transform.position = objectPosition;
+        workerObject.transform.parent = gameObject.transform;   //sets created object as child
+        workerObject.transform.localPosition = workerLocalPos;
+        
         workersList.Add(workerObject);
     }
-}
+
+    void removeWorker() {
+        int lastIndex = workersList.Count - 1;
+        if (lastIndex < 0) {
+            return;
+        }
+
+        Destroy(workersList[lastIndex]);
+        workersList.RemoveAt(lastIndex);
+    }
+
+    public int getWorkerCount() {
+        return workersList.Count;
+    }
+}   
