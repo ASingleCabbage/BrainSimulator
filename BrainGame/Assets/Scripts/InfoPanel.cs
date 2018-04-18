449549033038367e33aaaec3c9141ce1591108dc
Singular Cabbage <daniellangamma@gmail.com>
2018-04-18 13:22:23 -0400

    Player controller fixed, also added some scene backgrounds
    Worker sprite is still in front of the death screen for some reason

--- BrainGame/Assets/Scripts/InfoPanel.cs
+++ BrainGame/Assets/Scripts/InfoPanel.cs
@@ -6,16 +6,6 @@ public class InfoPanel : MonoBehaviour {
     public GameObject infoText;
     public float displayTimeSeconds = 2.0f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
     public void displayText(string s) {
         GameObject textObject = Instantiate(infoText);
         textObject.GetComponent<UnityEngine.UI.Text>().text = s;
