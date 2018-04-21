84ebe845fb1e6e83da82e3814ec6c58a9854dbe7
Singular Cabbage <daniellangamma@gmail.com>
2018-04-21 16:19:48 -0400

    More assets and scripts
    Added some background assets and scripts to handle scene changes

--- BrainGame/Assets/Scripts/PlayerController.cs
+++ BrainGame/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+    private float speedMultiplier = 1.0f;
 
 	private Rigidbody2D rb2d;
 
@@ -16,7 +17,15 @@ public class PlayerController : MonoBehaviour {
 	void FixedUpdate()
 	{
 		float moveHorizontal = Input.GetAxis("Horizontal");
-		Vector2 movement = new Vector2 (moveHorizontal, 0) * speed;
+		Vector2 movement = new Vector2 (moveHorizontal, 0) * speed * speedMultiplier;
         gameObject.transform.Translate(movement * Time.deltaTime);
 	}
+
+    public void setSpeedMultiplier(float multiplier) {
+        if (multiplier >= 0.0f && multiplier <= 1.0f) {
+            speedMultiplier = multiplier;
+        } else {
+            Debug.LogWarning("Invalid parameter for setSpeedMultiplier " + multiplier);
+        }
+    }
 }
